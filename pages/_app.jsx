f9fac6d3d531ec6fe05fbc187b5f2d4623a04bf3
Ian Langworth <ian@langworth.com>
2022-05-04 20:27:26 -0700

    Fix more Next.js complaints

---
+++ pages/_app.jsx
@@ -0,0 +1,17 @@
+import Head from "next/head"
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,10 +5,6 @@ export default function Document() {
     <Html>
       <Head>
         <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
         <link
           rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
